d1cad48862a20e64057d7803655ff6960629d130
alexandrep <pottiezalex@hotmail.com>
2023-07-27 10:09:18 +0200

    Envoie initial du FE

---
+++ FE/tuto.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html class="no-js" lang="">
+
+<head>
+  <meta charset="utf-8">
+  <title></title>
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <meta property="og:title" content="">
+  <meta property="og:type" content="">
+  <meta property="og:url" content="">
+  <meta property="og:image" content="">
+
+  <link rel="icon" href="/favicon.ico" sizes="any">
+ <!-- <link rel="icon" href="/icon.svg" type="image/svg+xml"> -->
+  <link rel="apple-touch-icon" href="icon.png">
+
+  <link rel="stylesheet" href="css/normalize.css">
+  <link rel="stylesheet" href="css/style.css">
+
+  <link rel="manifest" href="site.webmanifest">
+  <meta name="theme-color" content="#fafafa">
+
+  <link href="css/bootstrap.css" rel="stylesheet">
+  <link href="css/main.css" rel="stylesheet">
+</head>
+
+<body>
+  <!-- Navbar -->
+  <?php include "navbar.html";?>
+   
+  <!-- Add your site or application content here -->
+  <p>Ici le tuto pour récupérer l'archive Géovélo</p>
+  <script src="js/vendor/modernizr-3.12.0.min.js"></script>
+  <script src="js/app.js"></script>
+
+  <script src="js/bootstrap.js"></script>
+
+</body>
+
+</html>
