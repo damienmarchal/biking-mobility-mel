7b91061b11e2c15ab7d475bc5a6bc014ecfe581f
Damien Marchal <damien.marchal@univ-lille1.fr>
2023-07-30 21:49:23 +0200

    Add description in index.php

--- FE/index.php
+++ FE/index.php
@@ -3,7 +3,7 @@
 
 <head>
   <meta charset="utf-8">
-  <title></title>
+  <title>Campagne de collecte des traces de circulations vélos 2023</title>
   <meta name="description" content="">
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -13,7 +13,6 @@
   <meta property="og:image" content="">
 
   <link rel="icon" href="/favicon.ico" sizes="any">
- <!-- <link rel="icon" href="/icon.svg" type="image/svg+xml"> -->
   <link rel="apple-touch-icon" href="icon.png">
 
   <link rel="stylesheet" href="css/normalize.css">
@@ -29,14 +28,49 @@
 <body>
   <!-- Navbar -->
   <?php include "navbar.html";?>
-   
-  <!-- Add your site or application content here -->
-  <p>Ici le descriptif du projet</p>
+  
+  <div class="mx-auto" style="width: 90%;">
+
+  <p class="lead mt-4">
+    Depuis 2020 un challenge de vélo appelé #MaiAVélo est organisé à l'échelle nationnale. 
+    
+    
+    Pendant ce challenge les participants enregistrent tous leurs déplacement fait à vélo avec leur smartphone. 
+    Ces enregistrement permettent d'établir le classement mais ils sont une source d'information pour analyser la pratique du vélo sur un territoire.   
+
+    La Métropole Européenne de Lille, le département Nord ainsi que les Hauts-de-France se distinguent particulirenement à ce challenge et terminent 
+    largement premier. Il n'a pas fallut longtemps pour que l'idée germee au sein d'un collectif d'équipe #MaiAVélo que peut-être, grâce à la motivation de toutes les équipes régionales nous parviendrons à collecter suffisament de trace de circulation sur le territoire de la Métropole Européenne de Lille 
+    pour en extraire des informations intéressantes et utiles pour les acteurs de la cyclabilité territoriale (associations et opérateurs publiques). 
+  </p>
+  <p class="lead mt-4">    
+    Quelques jours plus tard, grâce à la bonne volonté de quelqu'un, ce site de collecte a vu le jour. 
+  </p>    
+  <p class="lead mt-4">    
+    Si vous circulez sur la MEL ou les communautés de communes aux alentours et que vous avez participé à #MaiAVélo, nous avons besoin de votre aide 
+    pour:</p>
+    <ul> 
+      <li> déposer sur notre serveur vos traces de circulation, où après anonymisation elle seront stoquées </li> 
+      <li> encourager vos connaissances ayant participé à #MaiAVélo à faire de même </li>
+    </ul>
+    </p> 
+
+    <div class="container mt-5 text-center">
+      <div class="row">
+        <div class="col-sm">
+          <a href="<a class="nav-link" href="depot.php">Déposer ses traces GPS</a>     <br><br>
+        </div>
+        <div class="col-sm">
+          <a href="<a class="nav-link" href="en-parler.php">Parlez en autour de vous</a>     <br><br>
+        </div>
+      </div>
+    </div>
+
+  </div>   
+  
   <script src="js/vendor/modernizr-3.12.0.min.js"></script>
   <script src="js/app.js"></script>
 
   <script src="js/bootstrap.js"></script>
-
 </body>
 
 </html>
