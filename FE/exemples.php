99fedd51c77d83f12391c16f05fc375f31230f6e
Damien Marchal <damien.marchal@univ-lille1.fr>
2023-07-30 21:50:15 +0200

    Add exemple.php pages and image.
    This page is to old exemple of use of the data. So that submitters know they
    share their data for "something".

---
+++ FE/exemples.php
@@ -0,0 +1,69 @@
+<!doctype html>
+<html class="no-js" lang="">
+
+<head>
+  <meta charset="utf-8">
+  <title>Campagne de collecte des traces de circulations vélos 2023: Exemples</title>
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <meta property="og:title" content="">
+  <meta property="og:type" content="">
+  <meta property="og:url" content="">
+  <meta property="og:image" content="">
+
+  <link rel="icon" href="/favicon.ico" sizes="any">
+  <link rel="apple-touch-icon" href="icon.png">
+
+  <link rel="stylesheet" href="css/normalize.css">
+  <link rel="stylesheet" href="css/style.css">
+
+  <link rel="manifest" href="site.webmanifest">
+  <meta name="theme-color" content="#fafafa">
+
+  <link href="css/bootstrap.css" rel="stylesheet">
+  <link href="css/main.css" rel="stylesheet">
+</head>
+
+<body>
+  <!-- Navbar -->
+  <?php include "navbar.html";?>
+  
+  <div class="mx-auto text-center mt-4" style="width: 90%;">    
+    Exemple d'usage des données qu'on fait les membres du collectifs. 
+
+    <div class="container mt-5">
+      <div class="row">
+        <div class="col-sm">
+         <img src="./img/exemple-santes.png"/>
+         <p>Comptage du nombre de trajet commençant, traversant ou terminant sur un territoire</p>
+        </div>
+        <div class="col-sm">
+         <img src="./img/exemple-heatmap.png"/>
+         <p>Carte de chaleur de la circulation</p>
+        </div>
+        <div class="col-sm">
+         <img src="./img/exemple-traffic.png"/>
+         <p>Analyse fine des trajets</p>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-sm">
+          ...
+        </div>
+        <div class="col-sm">
+          ...
+        </div>
+        <div class="col-sm">
+          ...
+        </div>
+      </div>
+    </div>   
+  
+  <script src="js/vendor/modernizr-3.12.0.min.js"></script>
+  <script src="js/app.js"></script>
+
+  <script src="js/bootstrap.js"></script>
+</body>
+
+</html>
